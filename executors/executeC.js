c8f6ed8fb9efdffae922abbcc7cae9ea6ff5fd74
gurjot <gurjotsaini53@gmail.com>
2023-12-24 23:01:49 +0530

    adding execute C

---
+++ executors/executeC.js
@@ -0,0 +1,30 @@
+const compiler = require("compilex");
+const envData = { OS: "windows", cmd: "gcc", options: { timeout: 10000 } }; // Use gcc for C
+const option = { stats: true };
+compiler.init(option);
+
+function executeC(code) {
+  return new Promise((resolve, reject) => {
+    compiler.compileC(envData, code, function (data) {
+      if (data.error) {
+        reject(data.error);
+      } else {
+        resolve(data.output);
+      }
+    });
+  });
+}
+
+function executeCWithInput(code, input) {
+  return new Promise((resolve, reject) => {
+    compiler.compileCWithInput(envData, code, input, function (data) {
+      if (data.error) {
+        reject(data.error);
+      } else {
+        resolve(data.output);
+      }
+    });
+  });
+}
+
+module.exports = { executeC, executeCWithInput };
--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,8 @@ const {
   executePythonWithInput,
 } = require("../executors/executePython");
 
+const { executeC, executeCWithInput } = require("../executors/executeC");
+
 router.post("/run", async (req, res) => {
   try {
     const code = req.body.code;
@@ -17,9 +19,11 @@ router.post("/run", async (req, res) => {
       if (lang === "cpp") output = await executeCPPWithInput(code, input);
       else if (lang === "python")
         output = await executePythonWithInput(code, input);
+      else if (lang === "C") output = await executeCWithInput(code, input);
     } else {
       if (lang === "cpp") output = await executeCPP(code);
       else if (lang === "python") output = await executePython(code, input);
+      else if (lang === "C") output = await executeC(code, input);
     }
 
     res.send(output);
